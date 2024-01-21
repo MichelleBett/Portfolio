aedf02b6982390049862d1c4b7546cab3947e803
MichelleBett <michelle.bett@strathmore.edu>
2024-01-21 23:23:34 +0300

    feat(interactivity): implement active class toggle on section click in skills section

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ navItems.forEach((item) => {
     navLinks.classList.toggle("nav-active");
   });
 });
+
+const sections = document.querySelectorAll('.section') 
+ 
+sections.forEach((section)=>{ 
+    section.addEventListener('click',()=>{ 
+  
+        sections.forEach((section) => { 
+            section.classList.remove('active') 
+        }) 
+        section.classList.add('active') 
+    }) 
+})
